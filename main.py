d2aa3d6ca4a31a0644d8cfb89a9db5f30b46fbac
Moonglum8 <michael_tagg@hotmail.com>
2026-01-18 16:05:51 +0000

    Replace main function to create and print a DataFrame using pandas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import pandas as pd
+
 def main():
-    print("Hello from hello-world!")
-
-
+    df = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
+    print(df)
+    
 if __name__ == "__main__":
     main()
